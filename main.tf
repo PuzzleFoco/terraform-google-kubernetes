15f4620e233ac25e1e4ce514287ccb993b0db9af
FabiusE <fabius.engel@msg.group>
2020-02-26 11:33:19 +0100

    New Branch

--- main.tf
+++ main.tf
@@ -35,25 +35,3 @@ resource "google_container_cluster" "primary" {
     update = "40m"
   }
 }
-
-resource "google_container_node_pool" "primary_preemptible_nodes" {
-    name        = "${var.gke_cluster_name}-node-pool"
-    location    = var.location
-    cluster     = google_container_cluster.primary.name
-    node_count  = var.node_count
-    project     = var.project
-
-    node_config {
-        preemptible     = true
-        machine_type    = var.machine_type
-
-        metadata = {
-            disable-legacy-endpoints = "true"
-        }
-
-        oauth_scopes = [
-            "https://www.googleapis.com/auth/logging.write",
-            "https://www.googleapis.com/auth/monitoring",
-        ]
-    }
-}

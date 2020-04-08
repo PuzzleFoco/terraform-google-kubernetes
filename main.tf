4cdc904b0acf2537af7a114a037e398c21aae03c
FabiusE <fabius.engel@msg.group>
2020-04-08 14:21:39 +0200

    Do not remove default node pool

--- main.tf
+++ main.tf
@@ -17,24 +17,24 @@ resource "google_container_cluster" "primary" {
     }
 }
 
-resource "google_container_node_pool" "primary_preemptible_nodes" {
-    name        = "${var.gke_cluster_name}-node-pool"
-    location    = var.location
-    cluster     = google_container_cluster.primary.name
-    node_count  = var.node_count
-    project     = var.project
+// resource "google_container_node_pool" "primary_preemptible_nodes" {
+//     name        = "${var.gke_cluster_name}-node-pool"
+//     location    = var.location
+//     cluster     = google_container_cluster.primary.name
+//     node_count  = var.node_count
+//     project     = var.project
 
-    node_config {
-        preemptible     = true
-        machine_type    = var.machine_type
+//     node_config {
+//         preemptible     = true
+//         machine_type    = var.machine_type
 
-        metadata = {
-            disable-legacy-endpoints = "true"
-        }
+//         metadata = {
+//             disable-legacy-endpoints = "true"
+//         }
 
-        oauth_scopes = [
-            "https://www.googleapis.com/auth/logging.write",
-            "https://www.googleapis.com/auth/monitoring",
-        ]
-    }
-}
+//         oauth_scopes = [
+//             "https://www.googleapis.com/auth/logging.write",
+//             "https://www.googleapis.com/auth/monitoring",
+//         ]
+//     }
+// }

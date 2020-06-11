00361e6b389957cf2648c39ee239b87eb1a0fbc1
Fabius Engel <Fabius.Engel@msg.group>
2020-06-11 19:06:15 +0200

    removed unnesseary things

--- README copy.md
+++
@@ -1,5 +0,0 @@
-# terraform-azurerm-azurekubernetes
-
-A module for a managed kubernetes in microsoft azure
-
-Use the Example in the examples folder for a first try
--- variables.tf
+++ variables.tf
@@ -1,14 +1,3 @@
-/*
- * Created Date: 14.01.2020
- * Author: Fabius Engel (fabius.engel@msg.group)
- * -----
- * Last Modified: 15.01.2020 10:34:11
- * Modified By: Fabius Engel (fabius.engel@msg.group)
- * -----
- * Copyright (c) 2020 msg nexinsure ag
- */
-
-
 variable "gke_cluster_name" {
     description = "Name of the GKE Cluster"
     type        = string
